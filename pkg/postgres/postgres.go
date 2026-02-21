2004462e7ff8de11e5980434d7d1678a664ed6d3
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-02-21 22:09:20 +0700

    feat: database connection

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,22 @@
+package postgres
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func StartPostgres() *gorm.DB {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Can not connect to database:%s", err.Error())
+	}
+
+	return db
+}
