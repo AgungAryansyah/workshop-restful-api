aed1ab0fef0db8f475ba6e462ef9b6a14b7413b8
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-02-27 22:58:41 +0700

    feat: restaurant repository

---
+++ internal/repository/restaurant.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"workshop-restful-api-backend/internal/entity"
+
+	"gorm.io/gorm"
+)
+
+type IRestaurantRepository interface {
+	CreateRestaurant(restaurant entity.Restaurant) error
+}
+
+type RestaurantRepository struct {
+	db *gorm.DB
+}
+
+func NewRestaurantRepository(db *gorm.DB) *RestaurantRepository {
+	return &RestaurantRepository{db}
+}
+
+func (r *RestaurantRepository) CreateRestaurant(restaurant entity.Restaurant) error {
+	result := r.db.Create(&restaurant)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *RestaurantRepository) GetRestaurants() ([]entity.Restaurant, error) {
+	restaurants := []entity.Restaurant{}
+
+	result := r.db.Find(&restaurants)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return restaurants, nil
+}
