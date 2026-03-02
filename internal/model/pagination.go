330e8dd1241dbe07138d6205f2d93516d400b11e
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-03-02 16:51:20 +0700

    feat: pagination models

---
+++ internal/model/pagination.go
@@ -0,0 +1,21 @@
+package model
+
+type Pagination struct {
+	Page  int `json:"page"`
+	Limit int `json:"limit"`
+}
+
+type PaginatedResponse[T any] struct {
+	Data       []T        `json:"data"`
+	Pagination Pagination `json:"pagination"`
+}
+
+func (p *Pagination) Check() {
+	if p.Page < 1 {
+		p.Page = 1
+	}
+
+	if p.Limit < 1 {
+		p.Limit = 10
+	}
+}
